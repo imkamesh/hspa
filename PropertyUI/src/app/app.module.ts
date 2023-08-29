13ea5399540528dd5622d719f17f3241b32fd5b3
imkamesh <kamesh85@gmail.com>
2023-08-29 20:12:24 +0530

    Register Form in UI

--- PropertyUI/src/app/app.module.ts
+++ PropertyUI/src/app/app.module.ts
@@ -1,13 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
 import { PropertyService } from './services/property.service';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from "./nav-bar/nav-bar/nav-bar.component";
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  //{path: '', component: PropertyListComponent},
+  {path: 'user/login', component: UserLoginComponent},
+  {path: 'user/register', component: UserRegisterComponent}
+]
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent, 
+    NavBarComponent,
+    UserLoginComponent,
+    UserRegisterComponent
+  ],
+  imports: [
+    BrowserModule,    
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    HttpClientModule    
+  ],
   providers: [PropertyService],
   bootstrap: [AppComponent],
 })
---
+++ PropertyUI/src/app/user/user-register/user-register.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/User';
+import Validation from 'src/app/utils/validation';
+
+@Component({
+  selector: 'app-user-register',
+  templateUrl: './user-register.component.html',
+  styleUrls: ['./user-register.component.css'],
+   
+})
+export class UserRegisterComponent implements OnInit { 
+  registrationForm:FormGroup = new FormGroup({
+    userName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    mobile: new FormControl('')
+  });
+  submitted = false;
+  user:User | undefined;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group(
+      {
+        userName: [null, [Validators.required]],
+        email: [null, [Validators.required, Validators.email]],
+        password: [null, [Validators.required, Validators.minLength(8)]],
+        confirmPassword: [null, [Validators.required]],
+        mobile: [null, [Validators.required, Validators.maxLength(10)]]
+      },
+      {
+        Validators: [Validation.match('password', 'confirmPassword')]
+      }
+    );
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.registrationForm.controls;
+  }
+  userData(): User {
+    return this.user = {
+      userName: this.f['userName'].value,
+      email: this.f['email'].value,
+      password: this.f['password'].value,
+      mobile: this.f['mobile'].value,
+    }
+  }
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.registrationForm.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.registrationForm.value, null, 2));
+  }
+  onReset(): void {
+    this.submitted = false;
+    this.registrationForm.reset();
+  }
+}
